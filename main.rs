08620e2ed2492aa0ac4cdd277dda5618ed52d38e
Lucas Root <lucasroot@Lucass-MacBook-Air-9.local>
2025-11-17 16:43:56 -0600

    started todo list

---
+++ main.rs
@@ -0,0 +1,13 @@
+let mut input = String::new();
+io::stdin().read_line(&mut input)?;
+
+//to_string() allocates new memory
+//inside loop?
+let clean_input = input.trim().to_string(); 
+
+let mut todos: Vec<String> = Vec::new()
+loop {
+    if input == 1 {
+
+    }
+}
