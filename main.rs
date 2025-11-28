f0a6a9f207407d0edb4cd17d875f7c5e4d4b64f9
cringey303 <lucas@the-b.org>
2025-11-27 20:07:15 -0800

    configured cargo and rust

--- main.rs
+++ main.rs
@@ -1,19 +1,45 @@
-let mut input = String::new();
-io::stdin().read_line(&mut input)?;
-
-//to_string() allocates new memory
-//inside loop?
-let clean_input = input.trim().to_string(); 
-
-let mut todos: Vec<String> = Vec::new()
-loop {
-    if input == 1 {
-
-    }
-}
-
+use serde::{Serialize, Deserealize};
+use std::io;
+#[derive(Serialize, Deserealize)]
 struct Note {
     id: u32,
     body: String,
     timestamp: String,
 }
+
+fn print_menu() {
+    println!("1. Add Note");
+    println!("2. Remove Note");
+    println!("3. Quit");
+    println!("Enter choice: ");
+}
+
+fn print_banner() {
+    println!("
+        ███╗   ██╗ ██████╗ ████████╗███████╗███████╗\n
+        ████╗  ██║██╔═══██╗╚══██╔══╝██╔════╝██╔════╝\n
+        ██╔██╗ ██║██║   ██║   ██║   █████╗  ███████╗\n
+        ██║╚██╗██║██║   ██║   ██║   ██╔══╝  ╚════██║\n
+        ██║ ╚████║╚██████╔╝   ██║   ███████╗███████║\n
+        ╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝");
+}
+
+fn main() -> io::Result<()> {
+    printBanner();
+    let mut notes: Vec<Note> = Vec::new();
+    loop {
+        printMenu();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+        let clean_input: i32  = input.trim().parse().expect("Not a valid choice");
+
+        if clean_input == 1 {
+            //add note
+        } else if clean_input == 2 {
+            //quit
+        }
+    }
+
+}
+
+
